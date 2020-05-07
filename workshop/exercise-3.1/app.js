99bb5768a464acf56fbe1c2cc3a9b457847f5252
jenny ma <jenny_lau_8@hotmail.com>
2020-05-06 20:45:26 -0400

    3.1

---
+++ workshop/exercise-3.1/app.js
@@ -0,0 +1,16 @@
+let body = document.querySelector('body');
+
+
+for (i = 0; i < 20; i++){
+    let button = document.createElement('button'); 
+
+    body.appendChild(button)
+    button.innerText = `Button ${i + 1}`
+    button.id = `button${i+1}`
+
+
+    button.addEventListener('click', function (){
+        button.style.backgroundColor = "green"
+    })
+}
+
